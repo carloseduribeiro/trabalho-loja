a8994aa1d8e8bf7205db4c78e9805190bb494c2a
Carlos Eduardo <carloseribeiro96@gmail.com>
2021-04-09 10:31:32 -0300

    Procedimento para remover o código da categoria nos produtos ao apagar a mesma adicionado;

--- Classes/Categorias.py
+++ Classes/Categorias.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+
 class Categorias:
 
     codigo = int
@@ -9,7 +11,7 @@ class Categorias:
 
     def salvar(self):
         ultimo_codigo = 0
-        # Caso o usuário não informe o código, cadastra um novo registro:
+        # Caso o usuario nao informe o codigo, cadastra um novo registro:
         if self.codigo <= 0:
             # Percorre o arquivo para pegar o ultimo codigo cadastrado:
             with open("banco/categorias.txt", "r") as dados_banco:
@@ -26,7 +28,7 @@ class Categorias:
                 else:
                     dados_banco.writelines(f"{self.codigo};{self.nome}")
 
-        # Caso o usuário informa o código, edita um registro existnte:
+        # Caso o usuÃ¡rio informa o cÃ³digo, edita um registro existnte:
         else:
             categorias = []
             # Salva os registros cadastrados em uma lista:
@@ -71,9 +73,17 @@ class Categorias:
                         categorias.append(cat)
                 dados_banco.writelines(categorias)
 
-            # Salva os produtos cadastrados numa lista:
-            # with open("banco/produtos.txt", "r") as dados_banco:
-            #     produtos_banco = dados_banco.readlines()
-            #
-            # with open("banco/produtos.txt", "w") as dados_banco:
-            #     for produto
+            # Remove o cÃ³digo da categoria removida nos produtos:
+            produtos_atualizados = []
+            with open("../banco/produtos.txt", "r") as dados_banco:
+                produtos_banco = dados_banco.readlines()
+
+            with open("../banco/produtos.txt", "w") as dados_banco:
+                for i in range(len(produtos_banco)):
+                    prod = produtos_banco[i].strip().split(";")
+                    prod[3] = '' if prod[3] == str(self.codigo) else prod[3]
+
+                    prod = str(';'.join(prod))
+                    prod += "\n" if i != len(produtos_banco) - 1 else ''
+                    produtos_atualizados.append(prod)
+                dados_banco.writelines(produtos_atualizados)
